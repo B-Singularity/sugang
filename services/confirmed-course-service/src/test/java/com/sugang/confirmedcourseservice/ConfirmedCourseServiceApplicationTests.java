7aaf42b1452f0e2c74bc49c5055270388774ad8c
B-singularity <skil1489@naver.com>
2025-07-01 15:32:58 +0900

    project setting init

---
+++ services/confirmed-course-service/src/test/java/com/sugang/confirmedcourseservice/ConfirmedCourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugang.confirmedcourseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmedCourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/registration-service/src/main/java/com/sugang/registrationservice/RegistrationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sugang.registrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RegistrationServiceApplication.class, args);
+  }
+}
